a5b931ad1fb71e564254405d90d022b55efd7fe8
Kurisu <poweredbykurisu@hotmail.com>
2019-04-26 21:04:00 +0100

    Implemented random timings to the things

---
+++ main.js
@@ -0,0 +1,9 @@
+function getRandomFloat(min, max) {
+    return Math.random() * (max - min) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+  }
